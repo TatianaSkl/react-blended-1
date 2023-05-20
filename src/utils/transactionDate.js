58d55902a5e0ae103511ac4fa5c1002d3cf86644
Antonina Grebeniuk <rebel.t2808@gmail.com>
2023-05-21 01:22:43 +0300

    Update transactionDate.js

--- src/utils/transactionDate.js
+++ src/utils/transactionDate.js
@@ -1,5 +1,7 @@
 import { format } from 'date-fns';
 
 export const transactionDate = date => {
-  return format(Date.parse(date), 'dd/MM/yyyy, h:mm aa');
+  return format(new Date(date), 'Pp');
 };
+
+// 'dd/MM/yyyy, h:mm aa'
