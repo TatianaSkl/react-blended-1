63f1855b6bf7b8b66fd46a25a91320205d81ee2f
TatianaSkl <116264242+TatianaSkl@users.noreply.github.com>
2023-05-22 18:12:41 +0300

    add task4

--- src/service/formatDate.js
+++ src/service/formatDate.js
@@ -1,5 +1,10 @@
 import { formatDistanceToNow } from 'date-fns';
+import { format } from 'date-fns';
 
-export const dateToNow = (date) => {
-    return formatDistanceToNow(new Date(date), { addSuffix: true });
-}
+export const formattedDate = date => {
+  return format(new Date(date), 'MM/dd/yyyy, HH:mm a');
+};
+
+export const dateToNow = date => {
+  return formatDistanceToNow(new Date(date), { addSuffix: true });
+};
