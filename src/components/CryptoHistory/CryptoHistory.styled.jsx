c660edbf44b2530a1e89a971a2bf300c2fa2389f
TatianaSkl <116264242+TatianaSkl@users.noreply.github.com>
2023-05-22 18:19:39 +0300

    fix

--- src/components/CryptoHistory/CryptoHistory.styled.jsx
+++ src/components/CryptoHistory/CryptoHistory.styled.jsx
@@ -1,36 +1,35 @@
 import styled from '@emotion/styled';
-import { theme } from 'styles';
 
 export const BaseTable = styled.table`
   width: 100%;
   border-collapse: collapse;
 
-  background-color: ${({ theme }) => theme.colors.white};
+  background-color: ${props => props.theme.colors.white};
   text-align: center;
 
-  box-shadow: ${({ theme }) => theme.shadows.small};
+  box-shadow: ${props => props.theme.shadows.small};
 `;
 
 export const THead = styled.thead`
-  color: ${({ theme }) => theme.colors.white};
-  background-color: ${({ theme }) => theme.colors.accent};
+  color: ${props => props.theme.colors.white};
+  background-color: ${props => props.theme.colors.accent};
   text-transform: uppercase;
 `;
 
 export const Td = styled.td`
   padding: 10px;
-  border: 1px solid ${({ theme }) => theme.colors.gray};
-  color: ${({ theme }) => theme.colors.dark};
+  border: 1px solid ${props => props.theme.colors.gray};
+  color: ${props => props.theme.colors.dark};
 `;
 
 export const Th = styled.th`
   padding: 10px;
-  border: 1px solid ${({ theme }) => theme.colors.gray};
-  color: ${({ theme }) => theme.colors.white};
+  border: 1px solid ${props => props.theme.colors.gray};
+  color: ${props => props.theme.colors.white};
 `;
 
 export const Tr = styled.tr`
   &:nth-of-type(odd) {
-    background-color: ${({ theme }) => theme.colors.light};
+    background-color: ${props => props.theme.colors.light};
   }
 `;
